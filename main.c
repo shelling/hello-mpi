00542aee93f34a7815f583156630cb0073c3547f
shelling <navyblueshellingford@gmail.com>
2010-06-14 01:52:14 +0800

    use MPI_Send() and MPI_Recv()

--- main.c
+++ main.c
@@ -31,7 +31,32 @@ int main ( int argc, char **argv ) {
   if (rank == 0) {
     printf("total is %d\n", total);
   }
-  
+
+  if ( rank < size-1 ) {
+    MPI_Send(
+      &rank,
+      1,
+      MPI_INT,
+      rank+1,            // rank of process to receive data
+      0,
+      MPI_COMM_WORLD
+    );
+  }
+
+  if ( rank > 0 ) {
+    int received_data;
+    MPI_Recv(
+      &received_data,
+      1,
+      MPI_INT,
+      rank-1,
+      0,
+      MPI_COMM_WORLD,
+      &status
+    );
+    printf("my rank is %d and  I received data from process %d\n", rank, received_data);
+  }
+
   MPI_Finalize();
 
   return 0;
