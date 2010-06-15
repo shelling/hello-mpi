0bc1d59d0a2da937f0fcbc469c5cd350954ff7c5
shelling <navyblueshellingford@gmail.com>
2010-06-15 12:28:38 +0800

    correct some typo and algorithm, mark code inspecting result as DEBUG section

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main ( int argc, char **argv ) {
 
   int rank, size;
   MPI_Status status;
-#
+ 
   MPI_Init( &argc, &argv );
 
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
@@ -69,17 +69,18 @@ int main ( int argc, char **argv ) {
   long double my_point = 0;
   long double prev, next;
 
-  for ( int time = 0; time<10; time++ ) {
+  for ( int time = 0; time<20000; time++ ) {
 
     switch (rank) {
       case 0:
-        my_point = 1.0;
+        my_point = sin( (long double) time * PI / 180.0 );
         MPI_Send( &my_point, 1, MPI_LONG_DOUBLE, rank+1, 0, MPI_COMM_WORLD );
         break;
 
       case 9:
         MPI_Send( &my_point, 1, MPI_LONG_DOUBLE, rank-1, 0, MPI_COMM_WORLD );
-        MPI_Recv( &my_point, 1, MPI_LONG_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status );
+        MPI_Recv( &prev, 1, MPI_LONG_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status );
+        my_point = ( prev - 0 ) / 2.0L;
         break;
 
       default:
@@ -89,7 +90,7 @@ int main ( int argc, char **argv ) {
         MPI_Recv( &prev, 1, MPI_LONG_DOUBLE, rank-1, 0, MPI_COMM_WORLD, &status );
         MPI_Recv( &next, 1, MPI_LONG_DOUBLE, rank+1, 0, MPI_COMM_WORLD, &status );
 
-        my_point = ( prev + next ) / 2.0L;
+        my_point = ( prev - next ) / 2.0L;
     
     }
 
@@ -97,11 +98,13 @@ int main ( int argc, char **argv ) {
 
     MPI_Gather( &my_point, 1, MPI_LONG_DOUBLE, fdtd_array, 1, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD );
 
+    #ifdef DEBUG
     if ( rank == 0 ) {
       printf( "timestep %d => ", time );
       array_inspect_ld( fdtd_array, 10 );
       printf("\n");
     }
+    #endif
 
   }
 
