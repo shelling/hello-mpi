90a1889a15a751105ffeea6e3a9cf770908c7547
shelling <navyblueshellingford@gmail.com>
2010-06-15 12:26:30 +0800

    define macro of PI

--- utils.h
+++ utils.h
@@ -1,3 +1,7 @@
+#ifndef PI
+#define PI 3.1415926535897932384626433832795L
+#endif
+
 void array_inspect_ld ( const long double * array, const int length ) {
   printf( "[ " );
   for ( int i=0; i<length; i++ ) {
